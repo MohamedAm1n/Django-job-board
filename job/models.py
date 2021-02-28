75dce2618b0c79f60232f866bb73af08235f63cb
amin <maamin97@yahoo.com>
2021-02-28 20:49:27 +0200

    add work and category models

--- job/models.py
+++ job/models.py
@@ -1,19 +1,33 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=25)
+    def __str__(self):
+        return self.name
+# 
 JOB_TYPE=(
     ('Full Time','Full Time'),
     ('Part Time','Part Time'),
 )
-class Job(models.Model):
+class Work(models.Model):
     title = models.CharField(max_length=100)
     job_type = models.CharField(max_length=15, choices=JOB_TYPE)
     description = models.TextField(max_length=1000)
     published_at = models.DateTimeField(auto_now=True)
-    vacancy = models.IntegerField(default=1)
-    salary = models.IntegerField(default=0)
+    vacancy = models.PositiveIntegerField(default=1)
+    salary = models.PositiveIntegerField(default=0)
     experience = models.IntegerField(default=1)
-    
-def __str__(self):
-    return self.title()
-    
+    cat = models.ForeignKey(Category,on_delete=models.CASCADE)
+    def __str__(self):
+        return  self.title
+# 
+# # End of class Job
+
+
+
+
+
+
+# # كنت بعمل الريليشن بين الوظيفة و الكاتيجوري
