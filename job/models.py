9b11113a0e58114bbdf53116585cb6bb5c0ce2c2
amin <maamin97@yahoo.com>
2021-03-02 23:59:20 +0200

    control image and install its package

--- job/models.py
+++ job/models.py
@@ -11,6 +11,10 @@ JOB_TYPE=(
     ('Full Time','Full Time'),
     ('Part Time','Part Time'),
 )
+def upload_image(instance,filename):
+    imagename , extension = filename.split(".")
+    return "jobs/%s.%s"%(instance.published_at,extension)
+pass
 class Work(models.Model):
     title = models.CharField(max_length=100)
     job_type = models.CharField(max_length=15, choices=JOB_TYPE)
@@ -20,6 +24,7 @@ class Work(models.Model):
     salary = models.PositiveIntegerField(default=0)
     experience = models.IntegerField(default=1)
     cat = models.ForeignKey(Category,on_delete=models.CASCADE)
+    image = models.ImageField(upload_to=upload_image)
     def __str__(self):
         return  self.title
 # 
