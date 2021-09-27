63c7f41a97d56452309c9a3905434fec5c9be07c
amin <maamin97@yahoo.com>
2021-09-27 19:11:59 +0200

    edit accounts models

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.db.models.deletion import CASCADE
+from django.db.models.fields import CharField
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Profile(models.Model):
+    user=models.OneToOneField(User, on_delete=models.CASCADE)
+    phone_number = models.CharField(max_length=15)
+    image = models.ImageField(upload_to="profile/")
+    city  = models.ForeignKey("City",related_name="uer_city",on_delete=CASCADE)
+    
+    
+    
+class City(models.Model):
+    name =models.CharField(max_length = 30)
+    
